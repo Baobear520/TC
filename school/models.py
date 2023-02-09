9cde6ebbeef3dc15e4aea400fe4b243bbf648870
Dima <aldmikon@gmail.com>
2023-02-09 09:26:28 +0800

    3rd commit Create models (student, enrollment, course, level), migrate to the database. ! Student(no 'contact'), enrollment(no 'coupon'), course( no 'teacher')!

--- school/models.py
+++ school/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.TextField(max_length=255)
+    last_name = models.TextField(max_length=255)
+    date_registered = models.DateField(auto_created=True)
+    
+class Level(models.Model):
+    title = models.TextField(max_length=255)
+
+class Course(models.Model):
+    title = models.TextField(max_length=255)
+    level = models.ForeignKey(Level,on_delete=models.CASCADE)
+    number_of_classes = models.IntegerField()
+    price = models.DecimalField('tuition_fee',max_digits=6,decimal_places=2)
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    date = models.DateField(auto_created=True)
+    course = models.ForeignKey(Course,on_delete=models.RESTRICT)
+
