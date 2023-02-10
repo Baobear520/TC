91ce17c4924343107bde7017017ecba32455ab6a
Dima <aldmikon@gmail.com>
2023-02-10 15:23:57 +0800

    4th commit Add __str__ representation for Student and Level model objects

--- school/models.py
+++ school/models.py
@@ -1,21 +1,27 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Student(models.Model):
     first_name = models.TextField(max_length=255)
     last_name = models.TextField(max_length=255)
     date_registered = models.DateField(auto_created=True)
     
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
+
 class Level(models.Model):
     title = models.TextField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.title
 class Course(models.Model):
     title = models.TextField(max_length=255)
     level = models.ForeignKey(Level,on_delete=models.CASCADE)
     number_of_classes = models.IntegerField()
-    price = models.DecimalField('tuition_fee',max_digits=6,decimal_places=2)
+    price = models.DecimalField('tuition_fee',max_digits=9,decimal_places=2)
 class Enrollment(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     date = models.DateField(auto_created=True)
-    course = models.ForeignKey(Course,on_delete=models.RESTRICT)
+    course = models.ForeignKey(Course,on_delete=models.PROTECT)
 
