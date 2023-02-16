468b772cc1721d303efe9254ae7bea5f063da588
Dima <aldmikon@gmail.com>
2023-02-16 08:31:36 +0800

    2nd admin committed 1.Change admin representation by using ModelAdmin class 2.Add list_display to each ModelAdmin 3.Add a temporary field "registered this year" into StudentAdmin 4.Use meta ordering by "last name" in Student model

--- school/admin.py
+++ school/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Student)
-admin.site.register(Enrollment)
-admin.site.register(Course)
-admin.site.register(Level)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','registered_this_year')
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('date_enrolled','student','course')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
--- school/models.py
+++ school/models.py
@@ -1,6 +1,7 @@
 import datetime
+from django.contrib import admin
 from django.db import models
-from django.utils import timezone
+from django.utils.html import format_html
 # Create your models here.
 class Student(models.Model):
     first_name = models.TextField(max_length=255)
@@ -8,7 +9,17 @@ class Student(models.Model):
     date_registered = models.DateField(auto_created=True)
     
     def __str__(self) -> str:
-        return self.first_name + ' ' + self.last_name
+        return f'{self.first_name} {self.last_name}'
+
+
+    @admin.display(boolean=True)
+    def registered_this_year(self):
+        now = datetime.date.today()
+        return now.year == self.date_registered.year
+
+    class Meta:
+        ordering = ['last_name']
+
 
 class Level(models.Model):
     title = models.TextField(max_length=255)
@@ -23,11 +34,11 @@ class Course(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
 class Enrollment(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    date = models.DateField(auto_created=True)
+    date_enrolled = models.DateField(auto_created=True)
     course = models.ForeignKey(Course,on_delete=models.PROTECT)
 
     def __str__(self) -> str:
-        return f'{self.date} {self.student}'
-    
+        return self.student
