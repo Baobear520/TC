6c69c54a0f68ada58c85b94dc4198a682644bf07
Dima <aldmikon@gmail.com>
2023-02-20 13:01:52 +0800

    3rd admin commit 1. Add django toolbar 2. Deleted temporary method "registered this year" in "student" model 3. Add "date hierarchy" into EnrollmentAdmin

--- school/admin.py
+++ school/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html,urlencode
+from django.urls import reverse
 from .models import *
 # Register your models here.
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name','last_name','registered_this_year')
+    list_display = ('first_name','last_name',)
+    
+
+    
 
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ('date_enrolled','student','course')
+    date_hierarchy = 'date_enrolled'
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
@@ -17,4 +23,5 @@ class CourseAdmin(admin.ModelAdmin):
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
     list_display = ('title',)
-
+    
+    
--- school/models.py
+++ school/models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.contrib import admin
 from django.db import models
-from django.utils.html import format_html
+
 # Create your models here.
 class Student(models.Model):
     first_name = models.TextField(max_length=255)
@@ -10,12 +10,6 @@ class Student(models.Model):
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-
-
-    @admin.display(boolean=True)
-    def registered_this_year(self):
-        now = datetime.date.today()
-        return now.year == self.date_registered.year
 
     class Meta:
         ordering = ['last_name']
@@ -39,6 +33,3 @@ class Enrollment(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     date_enrolled = models.DateField(auto_created=True)
     course = models.ForeignKey(Course,on_delete=models.PROTECT)
-
-    def __str__(self) -> str:
-        return self.student
