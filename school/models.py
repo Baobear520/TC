8bb3c5530ba1c4872aa67e498cafed4ca310bac3
Dima <aldmikon@gmail.com>
2023-03-03 16:34:50 +0800

    7th commit Add validation to model fields, overrode save method on Enrollment model

--- school/models.py
+++ school/models.py
@@ -1,5 +1,7 @@
 import datetime
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -10,6 +12,7 @@ class Student(models.Model):
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+    
     
 
     class Meta:
@@ -24,8 +27,12 @@ class Level(models.Model):
 class Course(models.Model):
     title = models.TextField(max_length=255)
     level = models.ForeignKey(Level,on_delete=models.CASCADE)
-    number_of_classes = models.IntegerField()
-    price = models.DecimalField('tuition_fee',max_digits=7,decimal_places=1)
+    number_of_classes = models.IntegerField(validators=[MinValueValidator(1)])
+    price = models.DecimalField('tuition fee',
+        max_digits=7,
+        decimal_places=1,
+        validators=[MinValueValidator(0)]
+        )
 
     def __str__(self) -> str:
         return self.title
@@ -33,20 +40,24 @@ class Course(models.Model):
     
 
 class Enrollment(models.Model):
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    student = models.ForeignKey(Student,on_delete=models.PROTECT)
     date_enrolled = models.DateField(auto_created=True)
     course = models.ForeignKey(Course,on_delete=models.PROTECT)
-    money_paid = models.DecimalField('Payment',max_digits=7,decimal_places=1,blank=True)
-    lessons = models.IntegerField('Number_of_lessons_left',blank=True)
-
+    money_paid = models.DecimalField('Payment',
+        max_digits=7,
+        decimal_places=1,
+        validators=[MinValueValidator(0)])
+    lessons = models.IntegerField('Number of lessons left',
+        blank=True,
+        validators=[MinValueValidator(0)])
 
     def __str__(self) -> str:
         return str(self.date_enrolled)
 
-    def save(self):
+    def save(self,*args,**kwargs):
         if not self.lessons:
             self.lessons = self.course.number_of_classes
-        if not self.money_paid:
-            self.money_paid = self.course.price
-        return super().save()
+        if self.student.date_registered > self.date_enrolled:
+            raise ValidationError('Enrollment date cannot be eairlier than registration date.')
+        return super().save(*args,**kwargs)
     
