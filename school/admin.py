ae78c49b302e71ded740b86650928374edf1aa2e
Dima <aldmikon@gmail.com>
2023-03-03 17:33:42 +0800

    9th admin commit 1. Add LevelInline class 2. Add formfield_overrides to all Inline classes 3. Change get_queryset method in Course model (     now counts all the enrollments with number of lessons greater than 0) 4. Add filter by course on Enrollment changelist page

--- school/admin.py
+++ school/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import TextInput, Textarea
 from django.db.models import Q, Count, OuterRef, Subquery, Value, F, Exists
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
@@ -10,12 +11,17 @@ class StudentInline(admin.TabularInline):
     model = Enrollment
     min_num = 1
     max_num = 2
+    extra = 0
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'is_enrolled')
     inlines = [StudentInline]
     search_fields = ('first_name','last_name')
-    
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
+
     @admin.display(ordering='is_enrolled',boolean=True)
     def is_enrolled(self, student):
         return student.is_enrolled
@@ -39,28 +45,46 @@ class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ('date_enrolled', 'student',
                     'course', 'money_paid', 'lessons')
     date_hierarchy = 'date_enrolled'
-    list_filter = ('student',)
+    list_filter = ('student','course')
     list_editable = ('lessons', 'money_paid')
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'number_of_current_enrollments')
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
+
 
     @admin.display(ordering='number_of_current_enrollments')
     def number_of_current_enrollments(self, course):
         return course.number_of_current_enrollments
 
     def get_queryset(self, request):
-        current_year = datetime.datetime.now().year
         num = Count(
             'enrollment',
-            filter=Q(enrollment__date_enrolled__year=current_year)
+            filter=Q(enrollment__lessons__gte=1)
         )
         return super().get_queryset(request).annotate(
             number_of_current_enrollments=num)
 
 
+class LevelInline(admin.TabularInline):
+    model = Course
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':1, 'cols':30})},
+    }
+    min_num = 1
+    extra = 0
+    
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
     list_display = ('title',)
+    inlines = [LevelInline]
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':40})},
+    }
